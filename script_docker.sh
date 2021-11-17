86f82ae1a804d44dadb2381c27eaae60d91d97ab
Ubuntu <margarida.pias97@gmail.com>
2021-11-17 22:44:41 +0000

    change script_docker from debian to ubuntu

--- script_docker.sh
+++ script_docker.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-apt-cache policy docker-ce
-sudo apt install -y docker-ce
-docker run --name some-nginx -v /some/content:/usr/share/nginx/html:ro -d nginx
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+#move resolv.conf to correct location
+#sudo mv /home/gors/resolv.conf /etc
+
+
+#run nginx
+#tive de adicionar nameservers em /etc/resolv.conf para conseguir correr o comando abaixo
+sudo docker pull nginx
+sudo docker run --name my-nginx -v /home/gors/html:/usr/share/nginx/html:ro -p 8080:80 -d nginx
+
+#Browsertime
+#sudo docker pull sitespeedio/browsertime
+#sudo docker run --shm-size=1g --rm -v "$(pwd)":/browsertime sitespeedio/browsertime https://www.sitespeed.io/
+
+##mudar para outro
+# ssh -i /home/gors/g.rsa.txt 192.168.109.152
