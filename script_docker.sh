00c603775c843cae58e5c3312c66ab9752414830
Ubuntu <margarida.pias97@gmail.com>
2021-11-18 10:19:28 +0000

    nginx run commands, browsertime works

--- script_docker.sh
+++ script_docker.sh
@@ -6,18 +6,13 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-#move resolv.conf to correct location
-#sudo mv /home/gors/resolv.conf /etc
-
-
 #run nginx
-#tive de adicionar nameservers em /etc/resolv.conf para conseguir correr o comando abaixo
 sudo docker pull nginx
 sudo docker run --name my-nginx -v /home/gors/html:/usr/share/nginx/html:ro -p 8080:80 -d nginx
 
 #Browsertime
-#sudo docker pull sitespeedio/browsertime
-#sudo docker run --shm-size=1g --rm -v "$(pwd)":/browsertime sitespeedio/browsertime https://www.sitespeed.io/
+sudo docker pull sitespeedio/browsertime
+sudo docker run --shm-size=1g --rm -v "$(pwd)":/browsertime sitespeedio/browsertime http://192.168.109.151:8080
 
-##mudar para outro
+#mudar para outro
 # ssh -i /home/gors/g.rsa.txt 192.168.109.152
